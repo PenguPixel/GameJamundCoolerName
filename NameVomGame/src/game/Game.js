50fb8c1fb1b882822a8296c99e18fd6ab8087b56
Manuel Hinreiner <mhinreiner.mmt-b2014@fh-salzburg.ac.at>
2026-08-22 01:11:29 +0200

    feat: add minimal title scene

--- NameVomGame/src/game/Game.js
+++ NameVomGame/src/game/Game.js
@@ -5,6 +5,7 @@ import { InputAction } from "../core/constants/InputAction.js";
 import { AnimationController } from "../core/animation/AnimationController.js";
 import { SceneId } from "../core/constants/SceneId.js";
 import { Level_01 } from "./scenes/Level_01.js";
+import { TitleScene } from "./scenes/TitleScene.js";
 
 export class Game
 {
@@ -46,9 +47,14 @@ export class Game
     #setupScenes()
     {
         this.updateManager.add(this.sceneManager);
+
+        this.sceneManager.registerScene(SceneId.TITLE, () =>
+            new TitleScene(this.inputManager, this.updateManager, this.sceneManager));
+
         this.sceneManager.registerScene(SceneId.LEVEL_01, () =>
             new Level_01(this.inputManager, this.updateManager, this.sceneManager));
-        this.sceneManager.changeScene(SceneId.LEVEL_01);
+
+        this.sceneManager.changeScene(SceneId.TITLE);
     }
 
 }
