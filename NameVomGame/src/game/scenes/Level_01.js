54b25ccc43713b5897031e7ec089ea9832da0faa
CodingMuesli <codingmuesli@gmail.com>
2026-08-23 11:43:40 +0200

    add Level_00

--- NameVomGame/src/game/scenes/Level_01.js
+++ NameVomGame/src/game/scenes/Level_01.js
@@ -1,95 +1,64 @@
 import * as THREE from 'three';
 import { BaseLevelScene } from './BaseLevelScene.js';
-import { AssetId } from '../../core/constants/AssetId.js';
+import levelData from '../levels/data/level_00.json';
+import { SceneId } from '../../core/constants/SceneId.js';
 
 export class Level_01 extends BaseLevelScene
 {
     constructor(inputManager, updateManager, sceneManager, assetManager, audioManager, gameState)
     {
         super(inputManager, updateManager, sceneManager, assetManager, audioManager, {
-            cameraDeadZoneX: 3,
-            cameraDeadZoneZ: 3,
-            cameraFollowY: false,
-            bodyPosition: new THREE.Vector3(0, 0, 0),
+            cameraFov: 60,
+            cameraNear: 0.1,
+            cameraFar: 300,
+            cameraOffset: new THREE.Vector3(0, 10, 5),
+            cameraSmoothing: 5,
+            cameraDeadZoneX: 0,
+            cameraDeadZoneZ: 0,
+            cameraFollowY: true,
+            bodyPosition: new THREE.Vector3(0, 0, 7),
             spiritPosition: new THREE.Vector3(2, 0, 0),
+            nextSceneId: SceneId.LEVEL_05,
             gameState,
-            levelTitle: 'level 01'
+            levelTitle: 'Fractured Paths'
         });
 
+        this.scene.background = new THREE.Color(0x101218);
+
         this.#setupLights();
-        this.#setupEnvironment();
         this.#setupAssets();
-        this.#setupWorldPlane();
-        this.createGroundCollider(50, 50);
-    }
-
-    enterLevel()
-    {
-    }
-
-    exitLevel()
-    {
+        this.#setupPhysics();
     }
 
     #setupLights()
     {
-        const ambientLight = new THREE.AmbientLight(0xffffff, 1);
-        const pointLight = new THREE.PointLight(0xff00ff, 8);
-        pointLight.position.set(1, 1, 1);
-        pointLight.castShadow = true;
-        pointLight.shadow.mapSize.set(1024, 1024);
-        pointLight.shadow.camera.near = 0.1;
-        pointLight.shadow.camera.far = 50;
-        pointLight.shadow.bias = -0.0001;
-        pointLight.shadow.normalBias = 0.02;
+        const ambientLight = new THREE.AmbientLight(0xffffff, 3);
+        const directionalLight = new THREE.DirectionalLight(0x2999AD, 9);
+        directionalLight.position.set(5, 10, 5);
+        directionalLight.castShadow = true;
+        directionalLight.shadow.mapSize.set(1024, 1024);
+        directionalLight.shadow.camera.left = -15;
+        directionalLight.shadow.camera.right = 15;
+        directionalLight.shadow.camera.top = 15;
+        directionalLight.shadow.camera.bottom = -15;
+        directionalLight.shadow.camera.near = 0.1;
+        directionalLight.shadow.camera.far = 50;
+        directionalLight.shadow.bias = -0.0001;
+        directionalLight.shadow.normalBias = 0.02;
 
-        this.scene.add(ambientLight, pointLight);
+        this.scene.add(ambientLight, directionalLight);
     }
 
-    #setupEnvironment()
-    {
-        const grid = new THREE.GridHelper(20, 20);
-        this.scene.add(grid);
-    }
 
     #setupAssets()
     {
-        const chest = this.assetManager.createInstance(AssetId.CHEST);
-        chest.scale.setScalar(100);
-        chest.position.set(-2, 0, 0);
-        chest.rotation.y = Math.PI / 2;
-
-        this.scene.add(chest);
+        this.levelObjects = this.loadLevelData(levelData);
     }
 
-    #setupWorldPlane()
+
+    #setupPhysics()
     {
-        const worldTileSize = 10;
-        const gridSize = 5;
-        const worldTileCount = gridSize * gridSize;
-        const worldTileMeshes = this.assetManager.createInstancedMeshes(AssetId.WORLD1, worldTileCount);
-        const dummy = new THREE.Object3D();
-
-        for (let i = 0; i < worldTileCount; i++)
-        {
-            const row = Math.floor(i / gridSize);
-            const column = i % gridSize;
-            const offset = (gridSize - 1) / 2;
-
-            dummy.position.set(
-                (column - offset) * worldTileSize,
-                0,
-                (row - offset) * worldTileSize
-            );
-            dummy.scale.setScalar(0.001);
-            dummy.rotation.x = -Math.PI / 2;
-            dummy.updateMatrix();
-
-            worldTileMeshes.setMatrixAt(i, dummy.matrix);
-        }
-
-        worldTileMeshes.instanceMatrix.needsUpdate = true;
-
-        this.scene.add(worldTileMeshes);
+        //creates colliders only where editor objects such as tiles and walls are placed
+        this.createLevelObjectColliders(this.levelObjects);
     }
 }
